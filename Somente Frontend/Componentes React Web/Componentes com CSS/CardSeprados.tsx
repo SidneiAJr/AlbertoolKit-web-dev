8f37442fe4090282532e1cd2f39d47d748f54474
Sid Jr <sidalbertok@gmail.com>
2026-06-17 23:16:21 -0300

    Create CardSeprados.tsx

---
+++ Somente Frontend/Componentes React Web/Componentes com CSS/CardSeprados.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Card.css'
+
+const CardSeparados = () => {
+    return (
+        <div className='ContainerCard'>
+            <h1>asas</h1>
+            <div className='grid-cards'>
+                <div className='card-item'>
+                    <h2>Teste</h2>
+                    <p>Teste</p>
+                    <button className='btn'>teste</button>
+                </div>
+                <div className='card-item'>
+                    <h2>Teste</h2>
+                    <p>Teste</p>
+                    <button className='btn'>teste</button>
+                </div>
+                <div className='card-item'>
+                    <h2>Teste</h2>
+                    <p>Teste</p>
+                    <button className='btn'>teste</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardSeparados
