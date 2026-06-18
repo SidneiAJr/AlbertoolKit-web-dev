8f44a95e05a680dc6425a3c0a23ee5719cd8307d
Sid Jr <sidalbertok@gmail.com>
2026-06-17 22:22:37 -0300

    Create Logo.tsx

---
+++ Somente Frontend/Componentes React Web/Componentes com CSS/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Logojava.css'
+
+export const Logo = () => {
+  return (
+    <div className='ContainerLogo'>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" className='logojava'/>
+        <h1 className='ContainerTexto'>Seu Logo Aqui</h1>
+    </div>
+
+  )
+}
