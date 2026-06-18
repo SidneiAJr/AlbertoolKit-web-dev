6e3c1e47baa72352fad65fdfbd40ef267130ac70
Sid Jr <sidalbertok@gmail.com>
2026-06-17 22:21:59 -0300

    Create Texto.tsx

---
+++ Somente Frontend/Componentes React Web/Componentes com CSS/Texto.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Texto.css'
+
+const Titulocores = () => {
+  return (
+    <h1 className='texto1'>Exe<span className='texto2'>mplo</span></h1>
+  )
+}
+
+export default Titulocores
