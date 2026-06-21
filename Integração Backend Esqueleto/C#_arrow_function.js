8fef0386d21543b3582986219276217470a40fc7
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:50:49 -0300

    Create C#_arrow_function.js

---
+++ Integração Backend Esqueleto/C#_arrow_function.js
@@ -0,0 +1,19 @@
+const api = "localhost:5000" // Backend C#
+const api = "localhost:5001" // Backend C#
+const api = "localhost:44300" // Backend C#
+
+const login = async  () => {
+    
+}
+
+const cadastro = async  () => {
+    
+}
+
+const listartodos = async  () => {
+    
+}
+
+const listarID = async  () => {
+    
+}
