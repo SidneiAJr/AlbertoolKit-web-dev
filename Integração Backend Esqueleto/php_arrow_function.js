18c374e69cbe490007dce00f9786383eec95773d
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:55:25 -0300

    Update php_arrow_function.js

--- Integração Backend Esqueleto/php_arrow_function.js
+++ Integração Backend Esqueleto/php_arrow_function.js
@@ -1,17 +1,41 @@
 const api = "localhost:8000" // Backend PHP
 
 const login = async  () => {
-    
+    try {
+        
+     } catch (error) {
+        
+     }finally{
+
+     }
 }
 
 const cadastro = async  () => {
-    
+    try {
+        
+     } catch (error) {
+        
+     }finally{
+
+     }
 }
 
 const listartodos = async  () => {
-    
+    try {
+        
+     } catch (error) {
+        
+     }finally{
+
+     }
 }
 
 const listarID = async  () => {
-    
+    try {
+        
+     } catch (error) {
+        
+     }finally{
+
+     }
 }
