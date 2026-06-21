79e24967e84c4528d9faf3b75dd664971d306a19
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:53:21 -0300

    Update Nodejs_async_function.js

--- Integração Backend Esqueleto/Nodejs_async_function.js
+++ Integração Backend Esqueleto/Nodejs_async_function.js
@@ -1,21 +1,41 @@
 const api = "localhost:3000" // Backend Nodejs TS ou Js
 
 async function login() {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 async function cadastro() {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 async function update() {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 async function listaID() {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 async function () {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
