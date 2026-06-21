9b7e6184ae1f4c5748ca8fd63cd5be28fdf2bedd
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:54:59 -0300

    Update Springboot_arrow_function.js

--- Integração Backend Esqueleto/Springboot_arrow_function.js
+++ Integração Backend Esqueleto/Springboot_arrow_function.js
@@ -1,17 +1,41 @@
 const api = "localhost:8080" // Backend Spring
 
 const login = async  () => {
-    
+     try {
+        
+     } catch (error) {
+        
+     }finally{
+
+     }
 }
 
 const cadastro = async  () => {
-    
+    try {
+        
+     } catch (error) {
+        
+     }finally{
+        
+     }
 }
 
 const listartodos = async  () => {
-    
+     try {
+        
+     } catch (error) {
+        
+     }finally{
+        
+     }
 }
 
 const listarID = async  () => {
-    
+     try {
+        
+     } catch (error) {
+        
+     }finally{
+        
+     }
 }
