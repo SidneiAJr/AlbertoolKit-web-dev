08618d8d97f7e426a8ad1e6fbdfbb4754b95ddbb
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:43:27 -0300

    Create Nodejs.js

---
+++ Integração Backend Esqueleto/Nodejs.js
@@ -0,0 +1,21 @@
+const api = "localhost:3000" // Backend Nodejs
+
+async function login() {
+    
+}
+
+async function cadastro() {
+    
+}
+
+async function update() {
+    
+}
+
+async function listaID() {
+    
+}
+
+async function () {
+    
+}
