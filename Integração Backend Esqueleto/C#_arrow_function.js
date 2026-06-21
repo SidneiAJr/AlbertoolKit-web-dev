accb8269994bab47e5f92d72a62651c71fd58ee1
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:54:20 -0300

    Update C#_arrow_function.js

--- Integração Backend Esqueleto/C#_arrow_function.js
+++ Integração Backend Esqueleto/C#_arrow_function.js
@@ -3,17 +3,35 @@ const api = "localhost:5001" // Backend C#
 const api = "localhost:44300" // Backend C#
 
 const login = async  () => {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 const cadastro = async  () => {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 const listartodos = async  () => {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 const listarID = async  () => {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
+
+
