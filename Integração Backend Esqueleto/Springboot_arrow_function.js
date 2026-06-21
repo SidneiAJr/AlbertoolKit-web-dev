f9a34f3f91c8ff2917aa5985cd5382fa4cba0270
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:47:47 -0300

    Create Springboot_arrow_function.js

---
+++ Integração Backend Esqueleto/Springboot_arrow_function.js
@@ -0,0 +1,17 @@
+const api = "localhost:8080" // Backend Spring
+
+const login = async  () => {
+    
+}
+
+const cadastro = async  () => {
+    
+}
+
+const listartodos = async  () => {
+    
+}
+
+const listarID = async  () => {
+    
+}
