f95530bd0814aa5df674a0d4e36280a7693d11c6
Sid Jr <sidalbertok@gmail.com>
2026-06-21 11:47:16 -0300

    Create Nodejs_arrow_async_function.js

---
+++ Integração Backend Esqueleto/Nodejs_arrow_async_function.js
@@ -0,0 +1,17 @@
+const api = "localhost:3000" // Backend Nodejs TS ou Js
+
+const login = async  () => {
+    
+}
+
+const cadastro = async  () => {
+    
+}
+
+const listartodos = async  () => {
+    
+}
+
+const listarID = async  () => {
+    
+}
