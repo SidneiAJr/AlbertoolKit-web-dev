881b7b5a5da3b4d5170e2edd16413935a06b1149
Sid Jr <sidalbertok@gmail.com>
2026-08-08 13:23:50 -0300

    Create FuncReuAPI.ts

---
+++ Somente Frontend/React Native/components/Components Reutilizaveis/FuncReuAPI.ts
@@ -0,0 +1,14 @@
+// ✅ array — só valores
+export const APIsSemChave = [
+  'https://pokeapi.co/api/v2',
+  'https://rickandmortyapi.com/api',
+  'https://hp-api.onrender.com/api',
+  'https://ghibliapi.vercel.app',
+  'https://official-joke-api.appspot.com',
+  'https://viacep.com.br/ws',
+  'https://economia.awesomeapi.com.br',
+  'https://brasilapi.com.br/api/feriados/v1',
+  'https://brasilapi.com.br/api/banks/v1',
+  'https://brasilapi.com.br/api/ddd/v1',
+  'https://brasilapi.com.br/api/cnpj/v1',
+] as const
